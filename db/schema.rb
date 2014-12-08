c98d6ff4bfd85079e710b5e6b47d5de5806ee8ac
Matt Coleman <matt.k.coleman@gmail.com>
2014-12-08 12:46:05 -0500

    Added Name and Location

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141205221808) do
+ActiveRecord::Schema.define(version: 20141208173206) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 20141205221808) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "name"
+    t.string   "location"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
